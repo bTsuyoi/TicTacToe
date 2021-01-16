0560b5db61fa1af549ffee4443fd87843b3625d7
bTsuyoi <gubbala.saikishore@gmail.com>
2021-01-16 11:16:31 +0530

    Added: Functionality to check free space before move

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 public class TicTacToe{
+	public static String move = "notdone";
 	private static char player_choice;
 	private static char computer_choice;
 	public static Scanner sc= new Scanner(System.in); 
-	private static char [][] boardGame = new char [3][3];
+//	private static char [][] boardGame = new char [3][3];
 	public static char[] board;
 	public static char[] board() {
-		char[] board = new char[10];
+		board = new char[10];
 		for(int i=1; i<board.length; i++) {
 			board[i]=' ';
 		}
 		return board;
 	}
 	
+
 	public static void chooseLetter() {
 		
 		
@@ -27,25 +29,31 @@ public class TicTacToe{
 	}
 	
 	public static void showBoard(){
-		System.out.println("\n "+boardGame[0][0]+" | "+boardGame[0][1]+" | "+boardGame[0][2]+" ");
+		System.out.println("\n "+board[1]+" | "+board[2]+" | "+board[3]+" ");
 		System.out.println("-----------");
-		System.out.println(" "+boardGame[1][0]+" | "+boardGame[1][1]+" | "+boardGame[1][2]+" ");
+		System.out.println(" "+board[4]+" | "+board[5]+" | "+board[6]+" ");
 		System.out.println("-----------");
-		System.out.println(" "+boardGame[2][0]+" | "+boardGame[2][1]+" | "+boardGame[2][2]+" ");
+		System.out.println(" "+board[7]+" | "+board[8]+" | "+board[9]+" ");
 	}
 	
 	public static void toPlay(){
 		System.out.println("Which position you want to enter you next move.");
-		int index = sc.nextInt();	
-		if(index > 0 && index < board.length)
-		{
-			if(board[index] == ' ')
-				board[index] = player_choice;
+		while(move.equals("notdone")) {
+			int index = sc.nextInt();	
+			if(index > 0 && index < board.length)
+			{
+				if(board[index] == ' ') {
+					board[index] = player_choice;
+					move = "done";
+					}
+				else
+					System.out.println("Position is not empty.");
+					System.out.println("Choose available positions.");
+			}	
 			else
-				System.out.println("Position is not empty");
-		}	
-		else
-		System.out.println("Entered position is outside the board.");
+			System.out.println("Entered position is outside the board.");
+			System.out.println("Choose available positions present within board.");
+		}
 	}
 	public static void main(String[] args) {
 		
@@ -54,10 +62,11 @@ public class TicTacToe{
 		String choice = sc.nextLine();
 		player_choice=choice.charAt(0);
 		System.out.println("Welcome to TicTacToe Game");
-		char[] board = board();
+        board = board();
 		chooseLetter();
 		showBoard();
 		toPlay();
+		showBoard();
 		sc.close();
 	}
 }
