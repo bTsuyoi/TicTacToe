2e4bf5e34ebc2df7b29c425e5c25d41cf7080348
bTsuyoi <gubbala.saikishore@gmail.com>
2021-01-16 10:52:03 +0530

    Added: Functionality for player to make a move

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 public class TicTacToe{
-	private static String player_choice;
-	private static String computer_choice;
+	private static char player_choice;
+	private static char computer_choice;
+	public static Scanner sc= new Scanner(System.in); 
 	private static char [][] boardGame = new char [3][3];
 	public static char[] board;
 	public static char[] board() {
@@ -15,12 +16,12 @@ public class TicTacToe{
 	public static void chooseLetter() {
 		
 		
-		if (player_choice.equals("X")) {
-			computer_choice = "Y";
+		if (player_choice == 'X') {
+			computer_choice = 'Y';
 			System.out.println("Computer letter is: "+ computer_choice);		
 		}
 		else {
-			computer_choice = "X";
+			computer_choice = 'X';
 			System.out.println("Computer letter is: "+ computer_choice);
 		}
 	}
@@ -31,20 +32,32 @@ public class TicTacToe{
 		System.out.println(" "+boardGame[1][0]+" | "+boardGame[1][1]+" | "+boardGame[1][2]+" ");
 		System.out.println("-----------");
 		System.out.println(" "+boardGame[2][0]+" | "+boardGame[2][1]+" | "+boardGame[2][2]+" ");
-		
-	
 	}
 	
+	public static void toPlay(){
+		System.out.println("Which position you want to enter you next move.");
+		int index = sc.nextInt();	
+		if(index > 0 && index < board.length)
+		{
+			if(board[index] == ' ')
+				board[index] = player_choice;
+			else
+				System.out.println("Position is not empty");
+		}	
+		else
+		System.out.println("Entered position is outside the board.");
+	}
 	public static void main(String[] args) {
 		
-		Scanner sc= new Scanner(System.in); 
+		
 		System.out.println("Choice letter X or Y for player");
-		player_choice = sc.nextLine();
+		String choice = sc.nextLine();
+		player_choice=choice.charAt(0);
 		System.out.println("Welcome to TicTacToe Game");
 		char[] board = board();
 		chooseLetter();
 		showBoard();
-
+		toPlay();
 		sc.close();
 	}
 }
